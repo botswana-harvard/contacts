78d5d021b0851bfeb3e1bef50ad92cb1775be609
ckgathi <ckgathi@gmail.com>
2019-10-02 09:51:42 +0200

    update page

--- contacts/model_wapper/contact_model_wrapper.py
+++ contacts/model_wapper/contact_model_wrapper.py
@@ -5,3 +5,7 @@ class ContactModelWrapper(ModelWrapper):
 
     model = 'contacts.contact'
     next_url_name = 'home_url'
+    
+    @property
+    def department(self):
+        return self.object.department.name
